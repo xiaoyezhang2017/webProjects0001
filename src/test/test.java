3412ca76e166a0e5d1b4c9f9743c2bb7028f140c
xiaoyezhang2017 <34224497+xiaoyezhang2017@users.noreply.github.com>
2019-11-27 10:34:17 +0800

    新建提交

---
+++ src/test/test.java
@@ -0,0 +1,20 @@
+package test;
+
+public class test {
+    public static void main(String args[]){
+        son s = new son();
+        // son s2 = new son("son");
+    }
+}
+
+class f{
+    f(){
+        System.out.println("我是父类无参构造函数");
+    }
+    f(String s){
+        System.out.println("我是父类有参构造::函数："+s);
+    }
+}
+class son extends f{
+
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: huahua
+  Date: 2019/9/29
+  Time: 16:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  </body>
+</html>
